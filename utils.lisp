cb0d3b5876b9a724db7303a3771f3864dc751a68
Jesse Alama <jesse.alama@gmail.com>
2010-12-21 15:33:54 +0000

    Go loop.

--- utils.lisp
+++ utils.lisp
@@ -5,4 +5,11 @@
 (defun concat (&rest strings)
   (apply 'concatenate 'string strings))
 
+(defun first-n (lst n)
+  (loop 
+     for i from 1 upto n
+     for elt in lst
+     collecting elt into items
+     finally (return items)))
+
 ;;; utils.lisp ends here
