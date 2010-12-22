d8b3133415e091ef2bb8bfeff95f804a238c0c30
Jesse Alama <jesse.alama@gmail.com>
2010-12-22 01:51:03 +0000

    Cast a text as an array, and an array as a text.

--- utils.lisp
+++ utils.lisp
@@ -9,6 +9,21 @@
 (defun concat (&rest strings)
   (apply 'concatenate 'string strings))
 
+(defun lines-as-array (str)
+  "Treat the newline character in STR as a delimiter and make an array of strings."
+  (let ((split (split "\\n" str)))
+    (make-array (list (length split)) :initial-contents split)))
+
+(defun array->newline-delimited-string (array)
+  (let ((newline (make-string 1 :initial-element #\Newline)))
+    (reduce #'(lambda (s1 s2)
+		(concat s1 newline s2))
+	    array)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Lists
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun first-n (lst n)
   (loop 
      for i from 1 upto n
