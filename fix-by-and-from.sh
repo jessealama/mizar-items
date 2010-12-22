d3e9fee82adb8004ba183b64ecff73aa87e62863
Jesse Alama <jesse.alama@gmail.com>
2010-12-22 18:27:36 +0000

    Exit properly.

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-file=$1
+file=$1;
 
 cat $file \
     | tr '\n' '\0' \
     | gsed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' \
-    | tr '\0' '\n'
+    | tr '\0' '\n';
+
+exit $?
