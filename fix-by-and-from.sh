19d5cff929362ecca5675ff7017870fd446a0034
Jesse Alama <jesse.alama@gmail.com>
2011-01-14 09:49:46 +0100

    Try to ensure that all by/from references are on a single line.

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -4,6 +4,7 @@ file=$1;
 
 cat $file \
     | tr '\n' '\0' \
+    | ~/sources/mizar/mizar-items/null.pl \
     | gsed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' \
     | tr '\0' '\n';
 
