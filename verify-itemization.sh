b241d67ed1b98150a5c6621ceda0958a7de7f436
Jesse Alama <alama@mws.cs.ru.nl>
2011-01-28 11:54:37 +0100

    Print OK or show the item that fails, and how.

--- verify-itemization.sh
+++ verify-itemization.sh
@@ -2,21 +2,23 @@
 
 article=$1;
 
-cd $article;
+cp -R /mnt/sdb3/alama/itemization/$article /dev/shm/alama/itemization/$article;
+cd /dev/shm/alama/itemization/$article;
 items=`ls text/*.miz`;
 for item in $items; do
     accom -q -s -l $item > /dev/null 2> /dev/null ;
     if [[ $? -eq "0" ]]; then
 	verifier -q -s -l $item > /dev/null 2>/dev/null;
 	if [[ $? -eq "0" ]]; then
-	    echo "$item: ok";
+	    echo "$article:$item: ok";
 	else
-	    echo "$item: not ok";
+	    echo "$article:$item: not ok (verifier error)";
 	fi;
     else
-	echo "$item: not ok";
+	echo "$article:$item: not ok (accommodator error)";
     fi;
 done
-cd ..;
+rm -Rf /mnt/sdb3/alama/itemization/$article;
+mv /dev/shm/alama/itemization/$article /mnt/sdb3/alama/itemization/$article;
 
 exit 0;
