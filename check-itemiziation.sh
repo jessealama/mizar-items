7f0788f8fbf362599542fdc326a5667976749699
Jesse Alama <alama@mws.cs.ru.nl>
2011-02-03 14:16:27 +0100

    Use variables rather than hard-coded paths.
    The hard-coding is now confined to one place.

--- check-itemiziation.sh
+++ check-itemiziation.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 article=$1;
+article_in_harddisk=/mnt/sdb3/alama/itemization/$article;
+article_in_ramdisk=/dev/shm/alama/itemization/$article;
 
-cp -R /mnt/sdb3/alama/itemization/$article /dev/shm/alama/itemization/$article;
-cd /dev/shm/alama/itemization/$article
-num_items=`ls text/*.miz | wc --lines`
+if [[ ! -e $article_in_harddisk/text ]]; then
+    echo "Failure: there is no text subdirectory for $article!";
+    exit
+fi
+
+cp -R $article_in_harddisk $article_in_ramdisk;
+
+cd $article_in_ramdisk;
+
+num_items=`ls text/ckb*.miz | wc --lines`
 for item_num in `seq 1 $num_items`; do
     item="text/ckb$item_num";
     accom -q -s -l $item > /dev/null 2>&1;
@@ -14,19 +23,19 @@ for item_num in `seq 1 $num_items`; do
 	    exporter -q -s -l $item > /dev/null 2>&1;
 	    transfer -q -s -l $item > /dev/null 2>&1;
 	else
-	    rm -Rf /dev/shm/alama/itemization/$article;
-	    echo "$article item #$item_num: verifier errors";
+	    rm -Rf $article_in_ramdisk;
+	    echo "Failure: $article item #$item_num: verifier errors";
 	    exit 1;
 	fi
     else
-	rm -Rf /dev/shm/alama/itemization/$article;
-	echo "$article item #$item_num: accommodator errors";
+	rm -Rf $article_in_ramdisk;
+	echo "Failure: $article item #$item_num: accommodator errors";
 	exit 1;
     fi
 done
 
-rm -Rf /mnt/sdb3/alama/itemization/$article;
-mv /dev/shm/alama/itemization/$article /mnt/sdb3/alama/itemization/$article;
+rm -Rf $article_in_harddisk;
+mv $article_in_ramdisk $article_in_harddisk;
 echo "$article: ok"
 
 exit 0;
