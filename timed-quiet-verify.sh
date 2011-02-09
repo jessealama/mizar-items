22e23476b36ca33d729e65972422b758dff645bb
Jesse Alama <alama@mws.cs.ru.nl>
2011-02-09 19:54:28 +0100

    Set a timeout on quiet verification.

---
+++ timed-quiet-verify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+article=$1;
+/usr/bin/time --format="%U" /mnt/sdb3/alama/mizar-items/verify-quietly.sh $article 2>&1;
+#                                                                                           ^^^^
+# /usr/bin/time reports its output on standard error.  If you drop the
+# "2>&1", you will find that $verifier_time is equal to the empty
+# string, and you'll *see* the output of the time command printed on
+# the terminal where you're executing this script.
+exit $?;
