0c499fe23cfcc18514f956e0fc4111e1f78bd8fa
Jesse Alama <jesse.alama@gmail.com>
2011-02-16 02:31:12 +0000

    Patch in Josef's sweaty theorem and scheme brutalizer.

--- reduce.pl
+++ reduce.pl
@@ -3,7 +3,7 @@
 use strict;
 
 # my @item_kinds = ('Pattern', 'Definiens', 'Identify', 'RCFCluster', 'Constructor');
-my @item_kinds = ('Definiens', 'Identify', 'RCFCluster');
+my @item_kinds = ('Pattern', 'Definiens', 'Identify', '[RCF]Cluster', 'Constructor');
 # - Definiens: gather these from from XML, compare to what appers in dfs
 #
 # - Identification: similar to Definiens.
@@ -59,6 +59,100 @@ if (-e $article_in_ramdisk) {
     die "Failure: $article is already in the ramdisk -- why?";
 }
 
+## return three hash pointers - first for schemes, then theorems, then definitions
+sub ParseRef
+  {
+    my ($filestem) = @_;
+    open(REF,">$filestem.refx") or die "Unable to open an output filehandle for $filestem.refx in directory $gtopdir: $!";
+    my @refs = ({},{},{});
+    my $i = 0;
+    while(<REF>)
+      {
+	if(/syThreeDots/) 
+	  {
+	    $_ = <REF>; 
+	    m(/.*x=\"(\d+)\".*/) or die "bad REFX file";
+	    my $articlenr = $1;
+	    $_ = <REF>; 
+	    m(/.*x=\"(\d+)\".*/) or die "bad REFX file";
+	    my $refnr = $1;
+	    <REF>; <REF>;
+	    $refs[$i]->{"$articlenr:$refnr"} = ();
+	  }
+	if(/sySemiColon/)
+	  {
+	    $i++;
+	  }
+    }
+    close(REF);
+    die "Wrong number of ref kinds: $i" if($i != 3);
+    return \@refs;
+}
+
+## only callable with .eth or .esh; $refs is an array pointer of three hashes returned by ParseRef
+sub PruneRefXML
+{
+  my ($xml_elem,$file_ext,$filestem,$refs) = @_;
+  
+  my ($schs, $ths, $defs) = @$refs;
+  
+  my $res = 0;
+  
+  
+  my $xitemfile = $filestem . $file_ext;
+  if (-e $xitemfile) {
+    {
+      open(XML, $xitemfile) or die "Unable to open an output filehandle for $xitemfile in directory $gtopdir!";
+      local $/; $_ = <XML>;
+      close(XML);
+    }
+  } else {
+    print "nothing to trim for $xitemfile";
+    return;
+  }
+
+  my ($xmlbeg,$xmlnodes,$xmlend) = $_ =~ m/(.*?)([<]$xml_elem\b.*[<]\/$xml_elem>)(.*)/s;
+  if (defined $xmlbeg) {
+    ## call Mizar parser to get the tp positions
+    my @xmlelems = $xmlnodes =~ m/(<$xml_elem\b.*?<\/$xml_elem>)/sg; # this is a multiline match
+
+    open(XML1,">$filestem$file_ext") or die "Unable to open an output filehandle for $filestem$file_ext in directory $gtopdir: $!";
+    print XML1 $xmlbeg;
+
+    if ($file_ext eq '.eth') {
+      foreach my $elemnr (0 .. scalar(@xmlelems)-1) {
+	my $first_line = (split /\n/, $xmlelems[$elemnr] )[0];
+	      
+	$first_line =~ m/.*articlenr=\"(\d+)\".* nr=\"(\d+)\".* kind=\"([DT])\"/ or die "bad element $first_line";
+	      
+	my ($ref, $kind) = ( "$1:$2", $3);
+	my $needed = ($kind eq 'T')? $ths : $defs;
+	      
+	if ( exists $needed->{$ref}) {
+	  print XML1 $xmlelems[$elemnr];
+	  $res++;
+	}
+      }
+    } elsif ($file_ext eq '.esh') {
+      foreach my $elemnr (0 .. scalar(@xmlelems)-1) {
+	my $first_line = (split /\n/, $xmlelems[$elemnr] )[0];	      
+	$first_line =~ m/.*articlenr=\"(\d+)\".* nr=\"(\d+)\".*/ or die "bad element $first_line";
+	      
+	if ( exists $schs->{"$1:$2"}) {
+	  print XML1 $xmlelems[$elemnr];
+	  $res++;
+	}
+      }
+    } else {
+      die "bad extension $file_ext";
+    }
+
+    print XML1 $xmlend;
+    close(XML1);
+    return $res;
+  }
+}
+
 if (system ('cp', '-Rf', $article_on_harddisk, $ramdisk) != 0) {
   system ('rm', "-Rf", $article_in_ramdisk); # trash whatever is there
   die "Failure: error copying $article to the ramdisk: $!";
@@ -94,61 +188,33 @@ foreach my $item (@items_for_article) {
 
   print "Using a timeout of $timeout seconds", "\n";
 
-  my $item_evl = "$item.evl";
-  my $item_ev_tmp = "$item.\$ev";
-
-  # # reduce vocabularies
-  # my $irrvoc_result = system ("irrvoc -l $item > /dev/null 2>&1");
-  # my $irrvoc_exit_code = ($irrvoc_result >> 8);
-  # if ($irrvoc_exit_code == 0) {
-  #   # do nothing -- there were no non-redundant vocabularies
-  # } elsif ($irrvoc_exit_code == 1) {
-  #   unless (-e $item_ev_tmp) {
-  #     die "irrvoc exited with code 1, but it failed to generate a new evl file";
-  #   }
-  #   system ('mv', $item_ev_tmp, $item_evl) == 0
-  #     or die "Failure: can't move '$item_ev_tmp' to '$item_evl': $!";
-  # } else {
-  #     die "irrvoc died on item $item of article $article with exit code $irrvoc_exit_code!";
-  # }
-
-  # # reduce theorems and schemes
-  # system ("irrths -l $item > /dev/null 2>&1");
-  # my $irrths_result = system ('irrths', '-l', $item);
-  # my $irrths_exit_code = ($irrths_result >> 8);
-  # if ($irrths_exit_code == 0) {
-  #   # do nothing -- there were no non-redundant theorems/schemes
-  # } elsif ($irrths_exit_code == 1) {
-  #   unless (-e $item_ev_tmp) {
-  #     die "irrths exited with code 1, but it failed to generate a new evl file";
-  #   }
-  #   system ('mv', $item_ev_tmp, $item_evl) == 0
-  #     or die "Failure: can't move '$item_ev_tmp' to '$item_evl': $!";
-  # } else {
-  #     die "irrths died on item $item of article $article with exit code $irrths_exit_code!";
-  # }
+  # take cae of theorems and schemes first
+  my $parsed_ref = @{ParseRef ($item)};
+  PruneRefXML ('Scheme', 'esh', $item, $parsed_ref);
+  PruneRefXML ('Theorem', 'eth', $item, $parsed_ref);
 
   foreach my $item_kind (@item_kinds) {
 
-    my $extension = $item_to_extension{$item_kind};
-
-    if (-e "$item.$extension") {
-      print "brutalizing item kind $item_kind for item $item of article $article...", "\n";
-      my $exit_code = system ('/mnt/sdb3/alama/mizar-items/miz_item_deps_bf.pl', $item_kind, $extension, $item, $timeout);
-
-      my $exit_code = $exit_code >> 8;
-      my $err_message = $!;
-      if ($exit_code == 0) {
-	print "successfully minimized item kind $item_kind", "\n";
-	# system ('cp', "$item.$extension", "$item-needed-$item_kind");
+      my $extension = $item_to_extension{$item_kind};
+      
+      if (-e "$item.$extension") {
+	print "brutalizing item kind $item_kind for item $item of article $article...", "\n";
+	my $exit_code = system ('/mnt/sdb3/alama/mizar-items/miz_item_deps_bf.pl', $item_kind, $extension, $item, $timeout);
+	
+	my $exit_code = $exit_code >> 8;
+	my $err_message = $!;
+	if ($exit_code == 0) {
+	  print "successfully minimized item kind $item_kind", "\n";
+	  # system ('cp', "$item.$extension", "$item-needed-$item_kind");
+	} else {
+	  print "failure", "\n";
+	  system ('rm', "-Rf", "/dev/shm/alama/itemization/$article") == 0
+	    or die "Failure: error deleting $article from the ramdisk!";
+	  die "Failure: something went wrong brutalizing item kind $item_kind for $item of $article: the exit code was $exit_code and the error output was: $err_message";
+	}
       } else {
-	print "failure", "\n";
-	system ('rm', "-Rf", "/dev/shm/alama/itemization/$article") == 0
-	  or die "Failure: error deleting $article from the ramdisk!";
-	die "Failure: something went wrong brutalizing item kind $item_kind for $item of $article: the exit code was $exit_code and the error output was: $err_message";
+	print "Success: nothing to brutalize for items of kind $item_kind for item $item of article $article", "\n";
       }
-    } else {
-      print "Success: nothing to brutalize for items of kind $item_kind for item $item of article $article", "\n";
     }
   }
 }
