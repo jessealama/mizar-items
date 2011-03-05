e31bd8e72ca4ff864c3d27580685b656956dc2a7
Jesse <alama@mizar.cs.ualberta.ca>
2011-03-05 08:23:14 -0700

    More local config.

--- itemize.sh
+++ itemize.sh
@@ -6,10 +6,14 @@ if [[ $host -eq "mizar" ]]; then
     harddisk=/local/data/proofs/itemization;
     ramdisk=/dev/shm;
     image=/home/alama/mizar-items/mizar;
+    export PATH=/home/alama/mizsrc/7_11_07/bin:$PATH
+    export MIZFILES=/home/alama/mizsrc/7_11_07;
 else
     harddisk=/mnt/sdb3/alama/itemization;
     ramdisk=/dev/shm/alama/itemization;
     image=/mnt/sdb3/alama/mizar;
+    export PATH=/mnt/sdb3/alama/7.11.07_4.156.1112/bin:$PATH
+    export MIZFILES=/mnt/sdb3/alama/7.11.07_4.156.1112
 fi
 
 article_on_harddisk=$harddisk/$article;
