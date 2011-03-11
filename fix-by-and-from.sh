5642eccd005b5f85be82f9a4ca637f03921e9ee1
Jesse Alama <jesse.alama@gmail.com>
2011-03-10 17:08:42 -0700

    Fix conflict (I hope the days of this kind of fix-up are numbered...)

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -4,7 +4,7 @@ file=$1;
 
 cat $file \
     | tr '\n' '\0' \
-    | /Users/alama/sources/mizar/mizar-items/null.pl \
+    | /home/alama/mizar-items/null.pl \
     | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' \
     | tr '\0' '\n';
 
