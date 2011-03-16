60b02591f04fcf6b506d0507a5ec79adb9459493
Jesse Alama <jesse.alama@gmail.com>
2011-03-16 16:36:07 +0000

    Lots of further documentation and examples.

--- site.lisp
+++ site.lisp
@@ -30,8 +30,8 @@
 	 ((:span :class "menu")
 	  ((:a :href "/") "main")
 	  ((:a :href "/about") "about")
-	  ((:a :href "/random-item") "random item")
-	  ((:a :href "/random-path") "random path")))))
+	  ((:a :href "/random-item") "random-item")
+	  ((:a :href "/random-path") "random-path")))))
       ,@body)
      (:hr)
      ((:div :class "footer")
@@ -400,39 +400,109 @@ returning NIL."
     (:p "This site aims to illustrate fine-grained dependency
     information about " (:a :href "http://www.mizar.org" "the Mizar
     Mathematical Library") ", a large collection of formalized mathematical knowledge.")
-    (:h1 "How are dependencies computed?")
+    (:h1 "how are dependencies computed?")
     (:p "For more information about the method for computing this
     information and some applications of it, see:")
     (:ul
      (:li (str "&ldquo;") (:a :href "http://centria.di.fct.unl.pt/~alama/materials/preprints/dependencies-in-formal-mathematics.pdf" "Dependencies in formal mathematics") (str "&rdquo;"))
      (:li (str "&ldquo;") (:a :href "http://centria.di.fct.unl.pt/~alama/materials/preprints/premise-selection-for-mathematics-by-corpus-analysis-and-kernel-methods.pdf" "Premise selection for mathematics by corpus analysis and kernel methods") (str "&rdquo;")))
-    (:h1 "What exactly are &ldquo;items&rdquo;?")
-    (:p "An " (:b "item") " is a top-level piece of
+    (:h1 "what exactly are &ldquo;items&rdquo;?")
+    (:p "An " (:b "item") " is anything exported by a top-level piece of
     a " (:tt "MIZAR") " article.")
     (:p "Here are all the different kinds of items:")
     (:dl
      (:dt "theorem")
+     (:dd
+      (:p "A theorem is a proved mathematical statement.  For example (from FUNCT_1):")
+      ((:div :class "miz-example")
+     (:pre "
+theorem
+  [x,y] in f iff x in dom f & y = f.x
+proof
+  thus [x,y] in f implies x in dom f & y = f.x
+  proof
+    assume
+A1: [x,y] in f;
+    hence x in dom f by RELAT_1:def 4;
+    hence thesis by A1,Def4;
+  end;
+  thus thesis by Def4;
+end;"))
+     (:p "This theorem states a simple property of functions, viewed extensionally as graphs: the pair (" (:em "x,y") ") belongs to " (:em "f") " precisely when " (:em "x") " is in the domain of " (:em "f") " and " (:em "y") " is the value of " (:em "f") " at " (:em "x") ".  The theorem is followed by a proof, which refers to earlier definitions from the same article (that's what the " (:tt "Def4") " is), as well as a definition taken from another article ( " (:tt "RELAT_1:def 4") ")."))
      (:dt "lemma")
+     (:dd
+      (:p "A lemma is just like a theorem, but is lacks the keyword '" (:tt "theorem") "'.  Example (from ZFMISC_1):")
+      ((:div :class "miz-example")
+       (:pre "
+Lm1: {x} c= X iff x in X
+proof
+  x in {x} by TARSKI:def 1;
+  hence {x} c= X implies x in X by TARSKI:def 3;
+  assume
+A1: x in X;
+  let y;
+  thus thesis by A1,TARSKI:def 1;
+end;"))
+      (:p "Here, the lemma is called " (:tt "Lm1") ".  It says that the singleton set {" (:em "x") "} is included (in " (:tt "MIZAR") "-ese, that's '"(:tt "c=") "') in " (:em "X") " precisely when " (:em "x") " is a member of " (:em "X") ".  The statement of a lemma is geneally followed by a proof.  In the example above, the proof has six lines.  It uses two definitions defined in the article TARSKI: " (:tt "TARSKI:def 1") " and " (:tt "TARSKI:def 3") "."))
      (:dt "scheme")
+     (:dd
+      (:p "A scheme represnts a collection of theorems that are
+      substitution instances of some theorem-pattern.  For example:")
+      ((:div :class "miz-example")
+       (:pre "
+scheme
+  Extensionality { X,Y() -> set, P[set] } : X() = Y()
+provided
+A1: for x holds x in X() iff P[x] and
+A2: for x holds x in Y() iff P[x]
+proof
+A3: x in Y() implies x in X()
+  proof
+    assume x in Y();
+    then P[x] by A2;
+    hence thesis by A1;
+  end;
+  x in X() implies x in Y()
+  proof
+    assume x in X();
+    then P[x] by A1;
+    hence thesis by A2;
+  end;
+  hence thesis by A3,TARSKI:2;
+end;"))
+      (:p "This scheme, taken from " (:tt "XBOOLE_0") ", faciltates proofs of set equality via the principle of extensionality.  (" (:b "Note") ": in set theory extensionality is traditionally not given as a " (:em "scheme") ", but rather as a single (formula) axiom.  To be clear, in " (:tt "MIZAR") " that is also the case; see " ((:a :href "/item/tarski/theorem/1" :title "The axiom of extensionality in MIZAR") "the axiom in the foundational article TARSKI") ".  However, one can also profitably formulate extensionality as a scheme.  Extensionality-as-axiom is used to justify extensionality-as-scheme.)  In this example, the parameters of the scheme are terms " (:em "X") " and " (:em "Y") ", assumed to be sets (in " (:tt "MIZAR") "-ese, we say that that the terms " (:em "X") " and " (:em "Y") "have the type " (:tt "set") ", which is indicated by the arrow " (:tt "->") ") and a property " (:em "P") " of sets.  The scheme says that we are entitled to conclude that the terms " (:em "X") " and " (:em "Y") " are equal if we have proved two things:")
+      (:ul
+       (:li "for every " (:em "x") " we have that " (:em "x") " is in " (:em "X") "iff the property " (:em "P") " holds of " (:em "x") ", and") 
+       (:li "for every " (:em "x") " we have that " (:em "x") " is in " (:em "X") "iff the property " (:em "P") " holds of " (:em "x") ".") )
+
+      (:p "The proof that follows is a justification that indeed " (:em "X") " and " (:em "Y") " are equal under these conditions.  (The reference to " (:tt "TARSKI:2") " is a reference to the " (:em "axiom") " of extensionality in Tarski-Grothendieck set theory.)"))
      (:dt "constructor")
+     (:dd (:em "(definition and exmaple forthcoming)"))
      (:dt "pattern")
+     (:dd (:em "(definition and exmaple forthcoming)"))
      (:dt "deftheorem")
+     (:dd (:em "(definition and exmaple forthcoming)"))
      (:dt "definiens")
+     (:dd (:em "(definition and exmaple forthcoming)"))
      (:dt "cluster")
-     (:dt "identification"))
-    (:p "There are, further, two kinds of items.  The item kinds " (:em "theorem") "," (:em "lemma") "," (:em "scheme") "," (:em "cluster") ", and " (:em "identification") " are, for lack of a better term, " (:b "self-standing") " because and when a top-level block for one of thes kinds of items is processed by the " (:tt "MIZAR") " verifier, one and only one item is produced.  The four items kinds " (:em "constructor") "," (:em "pattern") "," (:em "deftheorem") ", and " (:em "definiens") " are, in general, not self-standing in this sense.  All of these items are produced by a " (:tt "MIZAR") "definition, but moreover, one " (:tt "MIZAR") " definition can produce " (:b "multiple") " items.  For example: the definition (taken from the article " ((:a :href "/article/xboole_0") (:tt "XBOOLE_0")) ")")
-    (:pre "definition
+     (:dd (:em "(definition and exmaple forthcoming)"))
+     (:dt "identification")
+     (:dd (:em "(definition and exmaple forthcoming)")))
+    (:p "There's another way to look at items.  The item kinds " (:em "theorem") "," (:em "lemma") "," (:em "scheme") "," (:em "cluster") ", and " (:em "identification") " are, for lack of a better term, " (:b "self-standing") " because and when a top-level block for one of thes kinds of items is processed by the " (:tt "MIZAR") " verifier, one and only one item is produced.  The four items kinds " (:em "constructor") "," (:em "pattern") "," (:em "deftheorem") ", and " (:em "definiens") " are, in general, not self-standing in this sense.  All of these items are produced by a " (:tt "MIZAR") "definition, but moreover, one " (:tt "MIZAR") " definition can produce " (:b "multiple") " items.  For example: the definition (taken from the article " ((:a :href "/article/xboole_0") (:tt "XBOOLE_0")) ")")
+    ((:div :class "miz-example")
+     (:pre "
+definition
   let X be set;
   attr X is empty means
   :Def1:
   not ex x being set st x in X;
-end;")
+end;"))
     (:p "is definition of empty set.  The " (:tt "MIZAR") "verifier produces " (:em "four") " items from this single definition: one constructor, one pattern, one definiens, and one deftheorem.  One can see this by viewing" ((:a :href "/fragment/xboole_0/2") "the view of the article fragment for this definition") ".")
-    (:h1 "Divergences from the official MIZAR Mathematical Library")
+    (:h1 "for the expert: divergences from the official MIZAR Mathematical Library")
     (:p "This site is based on MML version 4.156.1102
     and " (:tt "MIZAR") " binaries based on version 7.11.07.  For more information, consult the " (:tt "MIZAR") " homepage at")
-    :blockquote
-    ((:a :href "http://www.mizar.org/") "http://www.mizar.org/")
+    (:blockquote
+     ((:a :href "http://www.mizar.org/") "http://www.mizar.org/"))
     (:p "There are slight differences between the
     official MML and official binary releases available on
     the " (:tt "MIZAR") " homepage.  To process " (:tt "MIZAR") "
@@ -461,6 +531,26 @@ end;")
     ((:blockquote :style "font-style:oblique;")
      "Let X" (:sub "1") ", X" (:sub "2") ", " (str "&hellip;") ", X" (:sub "90") ", X" (:sub "91") " be sets such that " (str "&hellip;"))
     (:p "simply because there are too many variables: here, there are 91, but the hard-coded limit in the official " (:tt "MIZAR") " verifier is 90.")
+    (:h1 "getting the data")
+    (:p "If you like, you can download the data that this site uses and remix it or verify it your own way (e.g., by making your own visualizations of the data or by checking that the dependency graph is acyclic).")
+    (:ul
+     (:li ((:a :href "/ckb-ckb-depgraph" :title "Fragment dependency graph") "The fragment dependency graph")
+	  (:p "Dependency data among article fragments.  This is the heart of the whole site. Although this site emphasizes dependencies among items, what is in fact computed are dependencies among fragments.  Fragments are the human-understanable &lsquo;witnesses&rsquo; to dependencies between items.")
+	  (:p "The file is a list of space-separated lines.  A line")
+	  (:blockquote
+	   (:tt "node-A node-B"))
+	  (:p "is an edge in the graph and means: " (:em "node-A immediately depends on node-B") ".  The syntax of nodes in this graph is ")
+	  (:blockquote
+	   (:tt (str "&lang;article-name&rang;:&lang;fragment-number&rang;"))))
+     (:li ((:a :href "/mizar-item-ckb-table" :title "Mapping from framents to items") "The fragment-to-item table")
+	  (:p "Mapping between fragments and items.  For some kinds of items, this is a one-to-one mapping.  For definition fragements, though, this is in general a one-to-many relation.")
+	  (:p "The dependency information that is presented on this site has a simple mathematical characterization: viewing this table as a relation " (:em "R") " who domain contains fraements and whose range contains items, and the fragment dependency table above as the relation " (:em "S") " whose domain and range are fragments, the full dependency table is simply the composition of " (:em "R") " convervse with the composition of " (:em "S") " and " (:em "R") ".")
+	  (:p "The file is a list of space-seaprated lines.  A line")
+	  (:blockquote
+	   (:tt "node-A node-B"))
+	  (:p "is an edge in the graph and means: " (:em "node-A generates node-B") ". The syntax of nodes in this graph is ")
+	  (:blockquote
+	   (:tt (str "&lang;article-name&rang;:&lang;fragment-number&rang; &lang;article-name&rang;:&lang;item-kind&rang;:&lang;item-number&rang;")))))
     (:h1 "behind the scenes")
     (:p "This site was implemented in " ((:a :href "http://en.wikipedia.org/wiki/Common_Lisp" :title "Common Lisp (wikipedia)") "Common Lisp") " and runs on the " ((:a :href "http://weitz.de/hunchentoot/") "hunchentoot") " web server.  If you're curious, you're welcome to " ((:a :href "https://github.com/jessealama/mizar-items/blob/hunchentoot-site/site.lisp") "browse the source code") " of the site.")))
 
@@ -686,7 +776,7 @@ end;")
 				      ckb-number))
 	       (item-html (file-as-string ckb-item-path)))
 	  (miz-item-html (str item-key)
-	    (:p (str item-key) " is toplevel item " (str ckb-number) " of article " (str article-name) ".")
+	    (:p (str item-key) " is toplevel fragment #" (str ckb-number) " of article " (str article-name) ".")
 	    (if (null (cdr items-for-ckb)) ; the CKB for this item generates only this item
 		(htm 
 		 (:p "This is the only item generated by that toplevel item."))
@@ -749,7 +839,7 @@ end;")
     (:blockquote
      "&ldquo;"
      ((:a :href "http://markun.cs.shinshu-u.ac.jp/mizar/mma.dir/2005/mma2005(2).pdf") "MIZAR: The first 30 years")
-     "&rdquo , by Roman Mutuszewski and Piotr Rudnicki, " (:em "Mechanized Mathematics and its Applications " (:b "4") "(1), (2005), pp. 3&ndash;24"))
+     "&rdquo , by Roman Mutuszewski and Piotr Rudnicki, " (:em "Mechanized Mathematics and its Applications") (:b "4") "(1), (2005), pp. 3&ndash;24")
     (:p "At the moment, this site is not really interactive: you can't work with " (:tt "MIZAR") " texts here.  If you'd like to get your hands dirty, you might want to visit " ((:a :href "http://mws.cs.ru.nl/mwiki/") "the " (:tt "MIZAR") " wiki") " project at Radboud University Nijmegen.")))
      
 
@@ -770,6 +860,12 @@ end;")
   (register-static-file-dispatcher "/mhtml.js"
 				   (mizar-items-config 'mhtml-js-path)
 				   "text/ecmascript")
+  (register-static-file-dispatcher "/ckb-ckb-depgraph"
+				   (mizar-items-config 'fragment-depdenency-graph)
+				   "text/plain")
+  (register-static-file-dispatcher "/mizar-item-ckb-table"
+				   (mizar-items-config 'item-to-fragment-path)
+				   "text/plain")
   ;; intro
   (register-exact-uri-dispatcher "/" #'emit-main-page)
   ;; about page
