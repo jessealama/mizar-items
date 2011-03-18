e04e2178f9d84f0c470ccb6eede5b80af487e941
Jesse Alama <jesse.alama@gmail.com>
2011-03-18 18:47:49 +0000

    Style information for the article listing.

--- site.lisp
+++ site.lisp
@@ -1068,8 +1068,8 @@ end;"))
 	 do
 	   (htm
 	    (:tr
-	     (:td (str article-name))
-	     (:td (str title)))))))
+	     (:td :style "article-name" (str article-name))
+	     (:td :style "article-title" (str title)))))))
     (:h1 "getting started")
     (:p "One can inspect " ((:a :href "/random-item") "a random item") " or " ((:a :href "/random-path") "search for a path between two random items") ".")
     (:p "One might also be interested in entering the vast space of " (:tt "MIZAR") " items by inspecting some landmarks.")
