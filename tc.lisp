01a25b64f5aa70a94e6aa5bd6f9eab34181bd0ba
Jesse Alama <jesse.alama@gmail.com>
2011-04-11 06:31:16 +0200

    First attempt at maintaining a singleton transitive closure data structure.

--- tc.lisp
+++ tc.lisp
@@ -26,3 +26,56 @@ information."
 	     (dotimes (k ,dimension)
 	       (unless (zerop (aref square-array j k))
 		 (setf (aref square-array i k) 1)))))))))
+
+(defclass item-transitive-closure ()
+  ((item-table :type hash-table
+	       :documentation "A mapping from all known items to natural numbers."
+	       :accessor item-table)
+   (adj-matrix :type (array bit)
+	       :accessor adj-matrix
+	       :documentation "An adjacent matrix representing the
+	       transitive closure of the graph
+	       *ITEM-DEPENDENCY-GRAPH-FORWARD*, with indices labelled
+	       by natural numbers according to ITEM-TABLE.")))
+
+(defparameter *the-tc* nil)
+
+(defmethod initialize-instance :around ((class item-transitive-closure)
+					&rest initargs
+					&key &allow-other-keys)
+  (or *the-tc*
+      (let ((item-table (make-hash-table :test #'equal))
+	    (next-index 0))
+	(flet ((register-item (item)
+		 (unless (gethash item item-table)
+		   (setf (gethash item item-table) next-index
+			 next-index (1+ next-index)))))
+	  (loop
+	     with tc = (call-next-method)
+	     with num-items = (count-items)
+	     with adj-matrix = (make-array (list num-items num-items)
+					   :element-type 'bit
+					   :initial-element 0)
+	     for item being the hash-keys in *item-dependency-graph-forward*
+	     for deps being the hash-values in *item-dependency-graph-forward*
+	     do
+	       (register-item item)
+	       (dolist (dep deps)
+		 (register-item dep))
+	       (let ((i (gethash item item-table)))
+		 (dolist (dep deps)
+		   (let ((j (gethash dep item-table)))
+		     (setf (aref adj-matrix i j) 1))))
+	     finally
+	       (setf (adj-matrix tc) adj-matrix
+		     (item-table tc) tc
+		     *the-tc* tc)
+	       (return tc))))))
+
+(defmethod item-index ((tc item-transitive-closure) item)
+  (gethash item (item-table tc)))
+
+(defmethod exists-path-from-to ((tc item-transitive-closure) from to)
+  (let ((i (item-index *the-tc* from))
+	(j (item-index *the-tc* to)))
+    (not (zerop (aref (adj-matrix tc) i j)))))
