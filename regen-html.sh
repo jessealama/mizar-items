e763a039abcf2d294fd835b77f5275d072502477
Jesse Alama <jesse.alama@gmail.com>
2011-04-14 13:21:33 +0100

    Parameterize some hard-coded paths.

--- regen-html.sh
+++ regen-html.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -
 
-for dir in `ls`; do
- echo $dir;
- cd $dir;
- find text -name "ckb*.xml1" ! -empty | parallel --jobs +0 "xsltproc --param linking 's' --param colored '1' --param proof_links '1' --param ajax_proofs '1' --stringparam source_article '$dir' ~/sources/mizar/xsl4mizar/MHTML/mhtml_main.xsl {} > {.}.html";
+html_stylesheet=~/sources/mizar/xsl4mizar/MHTML/mhtml_main.xsl
+
+for article in hidden tarski `cat $MIZFILES/mml.lar`; do
+ echo $article;
+ cd $article;
+ find text -name "ckb*.xml1" ! -empty | parallel --jobs +0 "xsltproc --param linking 's' --param colored '1' --param mizar_items '1' --param proof_links '1' --param ajax_proofs '0' --stringparam source_article '$article' $html_stylesheet {} > {.}.html";
   cd ..;
 done
+
+exit 0;
