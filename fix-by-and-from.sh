0a55010d89e2541e3675e98e0fb0b9504680fade
Jesse Alama <jesse.alama@gmail.com>
2011-04-21 13:19:11 +0200

    Watch out for line-initial white space.

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -7,7 +7,7 @@ null_script=/Users/alama/sources/mizar/mizar-items/null.pl;
 cat $file \
     | tr '\n' '\0' \
     | $null_script \
-    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' -e 's/[\d0]by/ by/g' -e 's/[\d0]from / from/g' \
+    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' -e 's/[\d0] *by/ by/g' -e 's/[\d0] *from / from/g' \
     | tr '\0' '\n';
 
 exit $?
