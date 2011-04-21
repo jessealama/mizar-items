60368c2820b04c9c44d151e7c73f6b159ced8601
Jesse Alama <jesse.alama@gmail.com>
2011-04-21 11:10:23 +0200

    Factor out the null script, and simplify dealing with line-initial by and from.

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -2,10 +2,12 @@
 
 file=$1;
 
+null_script=/Users/alama/sources/mizar/mizar-items/null.pl;
+
 cat $file \
     | tr '\n' '\0' \
-    | /home/alama/mizar-items/null.pl \
-    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' -e 's/\d0by \([^\d0]*[\d0]\)/ by \1/g' \
+    | $null_script \
+    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' -e 's/[\d0]by/ by/g' -e 's/[\d0]from / from/g' \
     | tr '\0' '\n';
 
 exit $?
