967e666e5d0bcf9c15cd2059003ea434a9d1f589
Jesse Alama <jesse.alama@gmail.com>
2011-04-21 11:05:13 +0200

    Deal with lines that start with "by".

--- fix-by-and-from.sh
+++ fix-by-and-from.sh
@@ -5,7 +5,7 @@ file=$1;
 cat $file \
     | tr '\n' '\0' \
     | /home/alama/mizar-items/null.pl \
-    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' \
+    | sed -e 's/ from\d0\([^\d0]*[\d0]\)/ from \1/g' -e 's/ by\d0\([^\d0]*[\d0]\)/ by \1/g' -e 's/\d0by \([^\d0]*[\d0]\)/ by \1/g' \
     | tr '\0' '\n';
 
 exit $?
