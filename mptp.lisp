18fcfc865ab476e77fa397352fd85e3402f0b120
Jesse Alama <jesse.alama@gmail.com>
2011-05-11 16:39:22 -0600

    Simplify MPTP mapping for the conjecture.

--- mptp.lisp
+++ mptp.lisp
@@ -134,11 +134,7 @@ the result of exporting the MML to MPTP.")
 	 (formula-as-axiom (name-formula-pair)
 	   (destructuring-bind (name . formula)
 	       name-formula-pair
-	     (named-formula-as-tptp-axiom name formula)))
-	 (formula-as-conjecture (name-formula-pair)
-	   (destructuring-bind (name . formula)
-	       name-formula-pair
-	     (named-formula-as-tptp-conjecture name formula))))
+	     (named-formula-as-tptp-axiom name formula))))
     (let* ((deps (gethash item *item-dependency-graph-forward*))
 	   (deps-named-formulas (reduce #'append (mapcar #'mptp-formula deps)))
 	   (deps-axioms (mapcar #'formula-as-axiom deps-named-formulas))
