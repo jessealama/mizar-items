6b4b142e7135f749bc16c8e3a84cd3d9557551c6
Jesse Alama <jesse.alama@gmail.com>
2011-05-26 15:45:25 +0200

    Compute the items that appear earlier in an article.
    The better to make hints with.

--- site-data.lisp
+++ site-data.lisp
@@ -380,3 +380,44 @@
        (push item (gethash (cons article fragment-number) table))
      finally
        (return table))) 
+
+(defgeneric earlier-items-from-same-article (item))
+
+(defmethod earlier-items-from-same-article ((item string))
+  (earlier-items-from-same-article (get-and-maybe-set-item-name item)))
+
+(defmethod earlier-items-from-same-article :around ((item symbol))
+  (if (gethash item *item-to-fragment-table*)
+      (call-next-method)
+      (error "The item '~a' doesn't appear in the item-to-fragment table!" (symbol-name item))))
+
+(defmethod earlier-items-from-same-article ((item symbol))
+  "A list of all items that appear earlier than ITEM that belong to
+  the same article as ITEM"
+  (let ((article (item-article item))
+	(fragment-number (gethash item *item-to-fragment-table*)))
+    (loop
+       for i from (1- fragment-number) downto 1
+       appending (gethash (cons article i) *fragment-to-item-table*) into earlier
+       finally (return earlier))))
+
+(defun initialize-items-data (mml-version)
+  (format t "Loading the dependency data...")
+  (setf *item-dependency-graph-forward*
+	(read-dependency-file (item-dependency-table-for-mml mml-version)))
+  (format t "done.~%")
+  (format t "Inverting the dependency table...")
+  (setf *item-dependency-graph-backward*
+	(invert-dependency-table *item-dependency-graph-forward*))
+  (format t "done.~%")
+  (format t "Loading the item-to-fragment table...")
+  (setf *item-to-fragment-table* (load-item-to-fragment-table mml-version))
+  (format t "done.~%")
+  (format t "Inverting the item-to-fragment table...")
+  (setf *fragment-to-item-table*
+	(invert-item-to-fragment-table *item-to-fragment-table*))
+  (format t "done.~%")
+  (format t "Loading the MPTP axiom file...")
+  (setf *mptp-table* (load-mptp-axioms-for-mml mml-version))
+  (format t "done.~%")
+  t)
