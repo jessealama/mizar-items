0e0ee6efd4e9c39666abf41e6ded2b70396741d4
Jesse Alama <jesse.alama@gmail.com>
2011-06-13 18:20:30 +0200

    Stub for adding an article to a library, with a duplicate-article condition.

--- library.lisp
+++ library.lisp
@@ -14,3 +14,21 @@
     :type string
     :accessor binary-version
     :initarg :binary-version)))
+
+(define-condition duplicate-article-error (error)
+  ((library
+    :initarg :library
+    :reader library)
+   (name
+    :initarg :name
+    :reader name))
+  (:documentation "An error representing the addition to a library of
+  an article that already exists under the same name as the proposed article.")
+  (:report
+   (lambda (condition stream)
+     (with-slots (library name)
+	 condition
+       (format stream "There is already an article in the library ~a with the name '~a'" library name)))))
+
+(defmethod add-article ((library library) (article article))
+  ())
