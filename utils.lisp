a11cc0e8470b4012266d3e7eb14057c7b564bc61
Jesse Alama <jesse.alama@gmail.com>
2011-07-29 13:00:20 +0200

    Some common functionality for running programs in SBCL and CCL.

--- utils.lisp
+++ utils.lisp
@@ -386,4 +386,44 @@ LIST; otherwise, return T and NIL."
 (defun exact-regexp (str)
   (concatenate 'string "^" str "$"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Running programs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun run-program (program args &key search input output error)
+  #+sbcl
+  (sb-ext:run-program program
+		      args
+		      :search t
+		      :input nil
+		      :output nil
+		      :error nil)
+  #+ccl
+  (ccl:run-program program
+		   args
+		   :input nil
+		   :output nil
+		   :error nil))
+
+(defun process-exit-code (process)
+  #+sbcl
+  (sb-ext:process-exit-code process)
+  #+ccl
+  (multiple-value-bind (status exit-code)
+      (ccl:external-process-status process)
+    (declare (ignore status))
+    exit-code))
+
+(defun process-output (process)
+  #+sbcl
+  (sb-ext:process-output process)
+  #+ccl
+  (ccl:external-process-output-stream process))
+
+(defun process-error (process)
+  #+sbcl
+  (sb-ext:process-error process)
+  #+ccl
+  (ccl:external-process-error-stream process))
+
 ;;; utils.lisp ends here
