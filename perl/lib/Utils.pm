ccabd025cebbf693e389ce3bc4a7f02072713afa
Jesse Alama <jesse.alama@gmail.com>
2012-01-27 13:42:22 +0000

    New utility.

--- perl/lib/Utils.pm
+++ perl/lib/Utils.pm
@@ -30,4 +30,26 @@ sub ensure_directory {
     (-d $dir) ? return 1 : return 0;
 }
 
+sub write_string_to_file {
+    my $string = shift;
+    my $path = shift;
+
+    if (! defined $string) {
+	croak ('Error: please supply a string.');
+    }
+
+    if (! defined $path) {
+	croak ('Error: please supply a path.');
+    }
+
+    open (my $fh, '>', $path)
+	or croak ('Error: unable to open an output filehandle to ', $path, ': ', $!);
+    print {$fh} ($string)
+	or croak ('Error: unable to print the given string to the given path: ', $!);
+    close $fh
+	or croak ('Error: unable to close the output filehandle to ', $path, ': ', $!);
+
+    return 1;
+}
+
 1;
