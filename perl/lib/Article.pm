d9a0f8079b3cc70512430e7bb25b16f28493107d
Jesse Alama <jesse.alama@gmail.com>
2012-03-05 14:36:43 +0000

    Write out the path.

--- perl/lib/Article.pm
+++ perl/lib/Article.pm
@@ -1372,6 +1372,7 @@ sub shortest_initial_verifiable_subsequence {
     # carp ('Computing shortest initial segment of ', scalar @elements, ' ', $root_element_name, ' elements (saving results under ', $path, '.)');
 
     my $article_name = $self->name ();
+    my $path = $self->get_path ();
 
     my %needed = ();
     my $index = scalar @elements - 1;
@@ -1390,7 +1391,7 @@ sub shortest_initial_verifiable_subsequence {
     my $verifiable = $self->verify (\%parameters);
 
     if ($verifiable != 1) {
-	croak ('Error: ', $article_name, ' is not initially verifiable!');
+	croak ('Error: ', $article_name, ' at ', $path, ' is not initially verifiable!');
     }
 
     while ($verifiable == 1 && $index > 0) {
