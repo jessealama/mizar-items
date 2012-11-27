8076a43c4db70d11fd2f6958718950ffe2df4194
Jesse Alama <jesse.alama@gmail.com>
2012-11-27 19:16:33 +0100

    Transform reservations.

--- src/lisp/reservations.lisp
+++ src/lisp/reservations.lisp
@@ -22,9 +22,27 @@
     (wsmparser article-path)
     (msmprocessor article-path)))
 
+(defun reservation-node-p (thing)
+  (let ((kind-attribute (xpath:evaluate "@kind" thing)))
+    (and (string= (xpath:evaluate "name (.)" thing) "Item")
+	 (not (xpath:node-set-empty-p kind-attribute))
+	 (string= (xpath:string-value kind-attribute) "Reservation"))))
+
 (defmethod without-reservations ((article-path pathname))
   (loop
      with items = (items article-path)
-     for item in (remove-if #'reservation-item-p items)
+     for item in (remove-if #'reservation-node-p items)
      collect (without-reservations item) into wrm-items
      finally (return wrm-items)))
+
+(defmethod without-reservation ((text-node dom:text))
+  text-node)
+
+(defmethod without-reservations ((node dom:element))
+  (let ((name (xpath:evaluate "name (.)" node))
+	(document (dom:owner-document node)))
+    (if (string= name "Fraenkel-Term")
+	(error "hey")
+	(let ((new-node (dom:create-element document name)))
+	  (dolist (child (children (xpath "*" node)) new-node)
+	    (dom:append-child new-node (without-reservations child)))))))
