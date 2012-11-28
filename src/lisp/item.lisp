50276433d9c60a62f8c13908a6736533c9204ba2
Jesse Alama <jesse.alama@gmail.com>
2012-11-28 16:11:17 +0100

    More item kinds.

--- src/lisp/item.lisp
+++ src/lisp/item.lisp
@@ -5,30 +5,21 @@
 (defclass item ()
   ((xml-node
     :accessor xml-node
+    :type dom:node
     :initarg :xml-node
     :initform (error "To create an item, a snippet of XML is required."))
-   (line
-    :reader line
-    :initarg :line
-    :initform nil
-    :type (or null integer))
-   (column
-    :reader column
-    :initarg :column
-    :initform nil
-    :type (or null integer))
    (children
+    :accessor children
     :type list
     :initform nil
-    :initarg :children
-    :accessor children))
+    :initarg :children))
   (:documentation "A piece of a Mizar article."))
 
 (defun make-item-from-xml (xml)
   (let ((name (xpath:evaluate "name (.)" xml))
 	(children (xpath "*" xml)))
-    (cond ((string= name "Item")
-	   (make-instance (let ((kind-attribute (xpath:evaluate "@kind" xml)))
+    (make-instance (cond ((string= name "Item")
+			  (let ((kind-attribute (xpath:evaluate "@kind" xml)))
 			    (if (xpath:node-set-empty-p kind-attribute)
 				(error "Don't know how to deal with an Item element that lacks a kind attribute.")
 				(let ((kind (xpath:string-value kind-attribute)))
@@ -46,26 +37,302 @@
 					 'theorem-item)
 					((string= kind "Regular-Statement")
 					 'regular-statement)
+					((string= kind "Private-Predicate-Definition")
+					 'private-predicate-definition)
+					((string= kind "Choice-Statement")
+					 'choice-statement)
+					((string= kind "Exemplification")
+					 'exemplification)
+					((string= kind "Generalization")
+					 'generalization)
+					((string= kind "Conclusion")
+					 'conclusion)
+					((string= kind "Loci-Declaration")
+					 'loci-declaration)
+					((string= kind "Attribute-Definition")
+					 'attribute-definition)
+					((string= kind "Cluster")
+					 'cluster)
+					((string= kind "Correctness-Condition")
+					 'correctness-condition)
+					((string= kind "Functor-Definition")
+					 'functor-definition)
+					((string= kind "Predicate-Definition")
+					 'predicate-definition)
+					((string= kind "Assumption")
+					 'assumption)
+					((string= kind "Property")
+					 'property)
+					((string= kind "Correctness")
+					 'correctness)
+					((string= kind "Pred-Antonym")
+					 'predicate-antonym)
+					((string= kind "Existential-Assumption")
+					 'existential-assumption)
 					(t
-					 (error "Don't know how to deal with Item nodes whose kind attribute is~%~%  ~a~%" kind))))))
-			  :xml-node xml
-			  :children (mapcar #'make-item-from-xml children)))
-	  ((string= name "Block")
-	   (make-instance 'mizar-block
-			  :xml-node xml
-			  :children (mapcar #'make-item-from-xml children)))
-	  ((string= name "Standard-Type")
-	   (make-instance 'standard-type
-			  :xml-node xml
-			  :children (mapcar #'make-item-from-xml children)))
-	  ((string= name "Variable")
-	   (make-instance 'variable-item
-			  :xml-node xml
-			  :children (mapcar #'make-item-from-xml children)))
-	  ((string= name "Variables")
-	   (mapcar #'make-item-from-xml children))
-	  (t
-	   (error "Don't know how to make an item from XML nodes named '~a'" name)))))
+					 (error "Don't know how to deal with Item nodes whose kind attribute is~%~%  ~a~%" kind)))))))
+			 ((string= name "Text-Proper")
+			  'text-proper)
+			 ((string= name "Block")
+			  'mizar-block)
+			 ((string= name "Type-List")
+			  'type-list-item)
+			 ((string= name "Type-Specification")
+			  'type-specification)
+			 ((string= name "Standard-Type")
+			  'standard-type)
+			 ((string= name "Scheme")
+			  'scheme-item)
+			 ((string= name "Schematic-Variables")
+			  'schematic-variables-item)
+			 ((string= name "Variable")
+			  'variable-item)
+			 ((string= name "Variables")
+			  'variables-item)
+			 ((string= name "Functor-Segment")
+			  'functor-segment-item)
+			 ((string= name "Predicate-Segment")
+			  'predicate-segment-item)
+			 ((string= name "Explicitly-Qualified-Segment")
+			  'explicitly-qualified-segment)
+			 ((string= name "Implicitly-Qualified-Segment")
+			  'implicitly-qualified-segment)
+			 ((string= name "Proposition")
+			  'proposition)
+			 ((string= name "Label")
+			  'label-item)
+			 ((string= name "Existential-Quantifier-Formula")
+			  'existential-quantifier-formula)
+			 ((string= name "Universal-Quantifier-Formula")
+			  'universal-quantifier-formula)
+			 ((string= name "Conditional-Formula")
+			  'conditional-formula)
+			 ((string= name "Biconditional-Formula")
+			  'biconditional-formula)
+			 ((string= name "Conjunctive-Formula")
+			  'conjunctive-formula)
+			 ((string= name "Disjunctive-Formula")
+			  'disjunctive-formula)
+			 ((string= name "Negated-Formula")
+			  'negated-formula)
+			 ((string= name "Predicative-Formula")
+			  'predicative-formula)
+			 ((string= name "Attributive-Formula")
+			  'attributive-formula)
+			 ((string= name "Private-Predicate-Formula")
+			  'private-predicate-formula)
+			 ((string= name "Arguments")
+			  'arguments-item)
+			 ((string= name "Simple-Term")
+			  'simple-term)
+			 ((string= name "Placeholder-Term")
+			  'placeholder-term)
+			 ((string= name "Private-Functor-Term")
+			  'private-functor-term)
+			 ((string= name "Straightforward-Justification")
+			  'straightforward-justification)
+			 ((string= name "Scheme-Justification")
+			  'scheme-justification)
+			 ((string= name "Conditions")
+			  'conditions-item)
+			 ((string= name "Local-Reference")
+			  'local-reference)
+			 ((string= name "Definition-Reference")
+			  'definition-reference)
+			 ((string= name "Thesis")
+			  'thesis)
+			 ((string= name "Attribute-Pattern")
+			  'attribute-pattern)
+			 ((string= name "Predicate-Pattern")
+			  'predicate-pattern)
+			 ((string= name "Operation-Functor-Pattern")
+			  'operation-functor-pattern)
+			 ((string= name "Locus")
+			  'locus)
+			 ((string= name "Loci")
+			  'loci-item)
+			 ((string= name "Definiens")
+			  'definiens)
+			 ((string= name "Existential-Registration")
+			  'existential-registration)
+			 ((string= name "Functorial-Registration")
+			  'functorial-registration)
+			 ((string= name "Adjective-Cluster")
+			  'adjective-cluster)
+			 ((string= name "Adjective")
+			  'adjective)
+			 ((string= name "Contradiction")
+			  'contradiction)
+			 ((string= name "Global-Choice-Term")
+			  'global-choice-term)
+			 ((string= name "it-Term")
+			  'it-term)
+			 ((string= name "Infix-Term")
+			  'infix-term)
+			 ((string= name "Circumfix-Term")
+			  'circumfix-term)
+			 ((string= name "Right-Circumflex-Symbol")
+			  'right-circumflex-symbol)
+			 ((string= name "Clustered-Type")
+			  'clustered-type)
+			 ((string= name "Single-Assumption")
+			  'single-assumption)
+			 ((string= name "Collective-Assumption")
+			  'collective-assumption)
+			 ((string= name "Redefine")
+			  'redefine)
+			 ((string= name "Theorem-Reference")
+			  'theorem-reference)
+			 ((string= name "Correctness")
+			  'correctness)
+			 ((string= name "NegatedAdjective")
+			  'negated-adjective)
+			 ((string= name "Provisional-Formulas")
+			  'provisional-formulas)
+			 (t
+			  (error "Don't know how to make an item from XML nodes named '~a'" name)))
+		   :xml-node xml
+		   :children (mapcar #'make-item-from-xml children))))
+
+(defclass redefine (item)
+  nil)
+
+(defclass correctness-condition (item)
+  nil)
+
+(defclass correctness (item)
+  nil)
+
+(defclass property (item)
+  nil)
+
+(defclass private-predicate-definition (item)
+  nil)
+
+(defclass mizar-term (item)
+  nil)
+
+(defclass global-choice-term (mizar-term)
+  nil)
+
+(defclass placeholder-term (mizar-term)
+  nil)
+
+(defclass simple-term (mizar-term)
+  nil)
+
+(defclass private-functor-term (mizar-term)
+  nil)
+
+(defclass it-term (mizar-term)
+  nil)
+
+(defclass infix-term (mizar-term)
+  nil)
+
+(defclass circumfix-term (mizar-term)
+  nil)
+
+(defclass right-circumflex-symbol (item)
+  nil)
+
+(defclass arguments-item (item)
+  nil)
+
+(defclass proposition (item)
+  nil)
+
+(defclass label-item (item)
+  nil)
+
+(defclass local-reference (item)
+  nil)
+
+(defclass theorem-reference (item)
+  nil)
+
+(defclass definition-reference (item)
+  nil)
+
+(defclass straightforward-justification (item)
+  nil)
+
+(defclass scheme-justification (item)
+  nil)
+
+(defclass formula-item (item)
+  nil)
+
+(defclass negated-formula (formula-item)
+  nil)
+
+(defclass predicative-formula (formula-item)
+  nil)
+
+(defclass attributive-formula (formula-item)
+  nil)
+
+(defclass assumption (item)
+  nil)
+
+(defclass existential-assumption (item)
+  nil)
+
+(defclass collective-assumption (item)
+  nil)
+
+(defclass single-assumption (item)
+  nil)
+
+(defclass contradiction (formula-item)
+  nil)
+
+(defclass private-predicate-formula (formula-item)
+  nil)
+
+(defclass existential-quantifier-formula (formula-item)
+  nil)
+
+(defclass choice-statement (item)
+  nil)
+
+(defclass exemplification (item)
+  nil)
+
+(defclass generalization (item)
+  nil)
+
+(defclass conclusion (item)
+  nil)
+
+(defclass loci-declaration (item)
+  nil)
+
+(defclass thesis (item)
+  nil)
+
+(defclass conditions-item (item)
+  nil)
+
+
+
+(defclass universal-quantifier-formula (formula-item)
+  nil)
+
+(defclass conditional-formula (formula-item)
+  nil)
+
+(defclass biconditional-formula (formula-item)
+  nil)
+
+(defclass conjunctive-formula (formula-item)
+  nil)
+
+(defclass disjunctive-formula (formula-item)
+  nil)
+
+(defclass text-proper (item)
+  nil)
 
 (defclass mizar-block (item)
   nil)
@@ -76,24 +343,43 @@
 (defclass standard-type (mizar-type)
   nil)
 
-(defclass qualified-segment (item)
-  ((variables
-    :type list
-    :accessor variables
-    :initform (error "To create a qualified segment, please supply a non-null list of variables.")
-    :initarg :variables)))
+(defclass clustered-type (mizar-type)
+  nil)
+
+(defclass type-list-item (item)
+  nil)
+
+(defclass type-specification (item)
+  nil)
+
+(defclass segment-item (item)
+  nil)
+
+(defclass functor-segment-item (segment-item)
+  nil)
+
+(defclass predicate-segment-item (segment-item)
+  nil)
+
+(defclass qualified-segment (segment-item)
+  nil)
 
 (defclass implicitly-qualified-segment (qualified-segment)
   nil)
 
 (defclass explicitly-qualified-segment (qualified-segment)
-  ((variable-type
-    :type item
-    :accessor variable-type
-    :initarg :type
-    :initform (error "To create an explicitly qualified segment, please supply a type for the qualified variables."))))
+  nil)
 
 (defclass variable-item (item)
+  nil)
+
+(defclass variables-item (item)
+  nil)
+
+(defclass scheme-item (item)
+  nil)
+
+(defclass schematic-variables-item (item)
   nil)
 
 (defclass regular-statement (item)
@@ -103,6 +389,57 @@
   nil)
 
 (defclass definition-item (item)
+  nil)
+
+(defclass functor-definition (item)
+  nil)
+
+(defclass predicate-definition (item)
+  nil)
+
+(defclass cluster (item)
+  nil)
+
+(defclass adjective-cluster (item)
+  nil)
+
+(defclass adjective (item)
+  nil)
+
+(defclass existential-registration (item)
+  nil)
+
+(defclass functorial-registration (item)
+  nil)
+
+(defclass definiens (item)
+  nil)
+
+(defclass attribute-definition (item)
+  nil)
+
+(defclass attribute-pattern (item)
+  nil)
+
+(defclass predicate-pattern (item)
+  nil)
+
+(defclass negated-adjective (item)
+  nil)
+
+(defclass provisional-formulas (item)
+  nil)
+
+(defclass predicate-antonym (item)
+  nil)
+
+(defclass operation-functor-pattern (item)
+  nil)
+
+(defclass locus (item)
+  nil)
+
+(defclass loci-item (item)
   nil)
 
 (defclass scheme-block-item (item)
@@ -116,6 +453,9 @@
 
 (defclass reservation-item (item)
   nil)
+
+(defun reservation-item-p (x)
+  (eql (type-of x) 'reservation-item))
 
 (defclass quantified-formula (item)
   ((matrix
