64c44c1dd7786ca57f4fc873f3a44385eca84c62
Jesse Alama <jesse.alama@gmail.com>
2013-05-04 14:10:15 +0200

    Use the new makefiles.

--- bin/wrm.pl
+++ bin/wrm.pl
@@ -7,6 +7,7 @@ use Getopt::Long;
 use File::Temp qw(tempfile);
 use File::Basename qw(basename dirname);
 use File::Spec;
+use File::Copy qw(copy);
 use XML::LibXML;
 use Cwd qw(cwd);
 use File::Copy qw(copy move);
@@ -16,28 +17,15 @@ use Pod::Usage;
 use charnames qw(:full);
 use Readonly;
 
-use FindBin qw($RealBin);
-use lib "$RealBin/../src/perl";
-use Utils qw(ensure_directory ensure_readable_file extension strip_extension);
-use Mizar;
-use Article;
-use Xsltproc qw(apply_stylesheet);
-
 Readonly my $LF => "\N{LF}";
+
+use FindBin qw($RealBin);
 
 my $opt_man = 0;
 my $opt_help = 0;
 my $opt_verbose = 0;
 my $opt_debug = 0;
 my $opt_check = 0;
-
-my $stylesheet_home = "$RealBin/../src/xslt";
-my $script_home = $RealBin;
-
-if (! -d $stylesheet_home) {
-    say {*STDERR} 'The stylesheet home directory at', $LF, $LF, '  ', $stylesheet_home, $LF, $LF, 'is not actually a directory.';
-    exit 1;
-}
 
 GetOptions (
     'man' => \$opt_man,
@@ -50,36 +38,44 @@ if (@ARGV != 1) {
     pod2usage (-exitval => 2);
 }
 
+my $make_dir = "$RealBin/../src/make";
+
+unless (-d $make_dir) {
+    say {*STDERR} 'Error: the makefile directory', $LF, $LF, '  ', $make_dir, $LF, $LF, 'is not a directory.';
+    exit 1;
+}
+
+my $wrm_makefile = "${make_dir}/wrm.makefile";
+
+if (-d $wrm_makefile) {
+    say {*STDERR} 'Error: the WRM makefile is actually a directory!';
+    exit 1;
+}
+
+unless (-e $wrm_makefile) {
+    say {*STDERR} 'Error: the WRM makefile does not exist.';
+}
+
+unless (-r $wrm_makefile) {
+    say {*STDERR} 'Error: the WRM makefile is not readable.';
+}
+
 my $article_arg = $ARGV[0];
 my $article_basename = basename ($article_arg, '.miz');
 my $article_dirname = dirname ($article_arg);
 my $article_miz = "${article_basename}.miz";
+my $article_wrm = "${article_basename}.wrm";
+my $article_tpr = "${article_basename}.tpr";
+my $article_verifier_stamp = "${article_basename}.verifier-stamp";
 
-if (! ensure_readable_file ($article_miz)) {
-    say {*STDERR} 'There is no readable file at ', $article_miz, '.';
-    exit 1;
-}
-
-my $article = Article->new (path => $article_miz,
-			    stylesheet_home => $stylesheet_home,
-			    script_home => $script_home);
-
-$article->without_reservations ();
+system ('make', "--makefile=${wrm_makefile}", $article_wrm);
 
 if ($opt_check) {
 
-    if ($article->accom ()) {
-	if ($article->is_verifiable ()) {
-	    exit 0;
-	} else {
-	    say {*STDERR} 'The WRM form of ', $article_miz, ' could not be verified.';
-	    exit 1;
-	}
-    } else {
-	say {*STDERR} 'The WRM form of ', $article_miz, ' could not be accommodated.';
-	exit 1;
-    }
-
+    system ('make', "--makefile=${wrm_makefile}", $article_tpr);
+    copy ($article_wrm, $article_tpr);
+    system ('mglue', $article_miz);
+    system ('make', "--makefile=${wrm_makefile}", $article_verifier_stamp);
 
 }
 
